e68f28225fac4fc5441243e33b47952d2a27383b
PRAMOD B <46969756+pramodaradhya@users.noreply.github.com>
2023-02-13 00:35:43 +0530

    basic

---
+++ Assets/scripts/singelton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monosingeltonGeneric<T> : MonoBehaviour where T:monosingeltonGeneric<T>
+{
+    private static T instance;
+    public static T Instance { get { return instance; } }
+
+    protected virtual void Awake()
+    {
+        if(Instance == null)
+        {
+            instance = (T)this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public class playerTank : monosingeltonGeneric<playerTank>
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+        public void Startattack()
+        {
+           
+        }
+    }
+
+    public class enemyTank:MonoBehaviour
+    {
+        private void Start()
+        {
+            playerTank.Instance.Startattack();
+        }
+    }
+}
